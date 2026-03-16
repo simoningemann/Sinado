a5b569ffb73c4262d42129c9eed5393e0172bbc9
siinch <simon.ingemanns@gmail.com>
2026-03-16 10:24:16 +0100

    init

---
+++ Sinado.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Sinado.Core;
+
+public class Class1
+{
+
+}
---
+++ Sinado.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Sinado.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
